bab5a3cc04a05b1e4f387c0472921aa6d765bcc3
Joshua Isaí Chicoj <81249233+jichicoj@users.noreply.github.com>
2022-05-31 00:20:29 -0600

    another changes

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ const consts = require('./constants')
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), () => {
-    console.log(`Server listening on http://localhost:${port}/`)
+    console.log(`Server listening on http://localhost:${app.get('port')}/`)
 })
